41826ed9d49d836ed8050b4c1ad047c82da87fbe
mpons <mpons@student.42lausanne.ch>
2023-03-30 18:57:26 +0200

    proxy with colors

--- proxy/parser.py
+++ proxy/parser.py
@@ -1,10 +1,19 @@
-def parse(data, port, origin):
-    if port == 5555: #c'est impossible en l'occurence mais c'est pour l'exemple
-        return
-    else:
-        print ("[", origin, ":", str(port), "]", data)
-        # print (":".join("{:02x}".format(ord(c)) for c in data))
+RESET = "\033[0m"
+RED = "\033[31m"
+GREEN =	"\033[32m"
+YELLOW = "\033[33m"
+BLUE = "\033[34m"
+MAGENTA = "\033[35m"
+CYAN = "\033[36m"
+WHITE = "\033[37m"
 
+def parse(data, port, origin):
+    if origin == "server":
+        print (BLUE, "[", origin, ":", str(port), "]", data, RESET)
+    else:
+        print (GREEN, "[", origin, ":", str(port), "]", data, RESET)
+
+# print (":".join("{:02x}".format(ord(c)) for c in data))
 #il y a un lf ( + un cr) en fin de data, ducoup c'est pour ca que ca print sur 2 lignes, faites un bail du genre pour print sur 1:
 #print ("[" + origin + ":" + str(port) + "]" + data, end='') je c meme pas si ca marche mais c'est un bail du genre pour sur
 
