df58b0c46435c7f152d6ac861611a26b483cbbad
eprei <93380002+eprei@users.noreply.github.com>
2023-03-31 15:10:17 +0200

    user in progress

--- user.cpp
+++ user.cpp
@@ -6,7 +6,7 @@ void			Client::user(Message *m){
 	std::cout << GREEN << ">\tuser function executed " << RESET <<"by client id: " << _id << "\t\t<" << std::endl;
 
 	if (m->params[0].size() < 4 || m->params[0].empty() || m->params[1].empty() || m->params[2].empty() || m->params[3].empty())
-		send_reply(461, this, _server, _nickname, "user", "", "");
+		send_reply(461, this, _server, "user", "", "", "");
 	else
 	{
 		_username = m->params[0];
@@ -22,3 +22,17 @@ void			Client::user(Message *m){
 	}
 
 }
+
+
+		this->setNickname(m->params[0]);
+		std::string msg = ":"; // formatMsgsUsers(_nickname, _username, getHostname());
+		msg.append(_server->getName());
+		msg.append(" ");
+		msg.append("99");
+		msg.append(" ");
+		msg.append("NICK " + _nickname + " has succesfully changed his nickname\r\n");
+		// msg.append("NICK " + _nickname + "\r\n");
+
+		std::cout << FC(YELLOW, "Server Reply to be sent:\n") << msg << std::endl;
+		if (send(getSocket(), msg.c_str(), msg.length(), 0) < 0)
+			perror("SEND FAILED");
